9b57e8be561d650d0fa40b3342c5cfe1c3b8f8bf
craigcompton <39470012+craigcompton@users.noreply.github.com>
2018-11-16 10:04:41 -0800

    Add files via upload
    Firday commits

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cod Fish Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Morning Star Farms Veggie Burger', false);
--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-// Dependencies
-// =============================================================
 var express = require("express");
-var path = require("path");
+var mysql = require("mysql");
+var orm = require("./config/orm.js");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
