9e1779f43c81fc7432357a3582b497ec8b13ebf1
Craig Compton <craigcomp@gmail.com>
2018-11-17 20:37:41 -0800

    jesus....

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
 var mysql = require("mysql");
 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}else{
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -13,6 +18,7 @@ var connection = mysql.createConnection({
   password: "",
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) throw err;
