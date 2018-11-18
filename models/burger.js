4513cdc998c0d7bfd7ab349cb132c6fab903b834
craigcompton <39470012+craigcompton@users.noreply.github.com>
2018-11-17 20:51:11 -0800

    Add files via upload

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
@@ -21,4 +22,29 @@ var burger = {
 };
 
 // Export the database functions for the controller (burgersController.js).
+=======
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb2) {
+    orm.all("burgers", function(res) {
+      cb2(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgersController.js).
+>>>>>>> 33f916f6b8fd76b892ccb06e557095e4255f140c
 module.exports = burger;
